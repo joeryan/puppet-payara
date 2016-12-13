deee1027740477abad4a047918ac2d09ca52279d
Ben Vanhaeren <ben.vanhaeren@vrt.be>
2016-12-13 09:41:10 +0100

    Remove java dependency. Managed by role

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,14 +1,8 @@
 class payara::install {
   package { 'payara-micro':
     ensure  => 'latest',
-    require => [Package['rotate'], Java::Oracle['8']]
+    require => Package['rotate'],
   }
-
   package { 'rotate': ensure => present }
-
-  java::oracle { '8':
-    ensure  => present,
-    version => 8,
-  }
 }
 
